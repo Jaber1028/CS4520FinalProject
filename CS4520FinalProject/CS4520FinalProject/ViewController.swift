491954e5fbfb4bdc400f0db73982352c67d3c6e4
Kenny Lin <lin.kenn@northeastern.edu>
2023-06-20 16:18:18 -0400

    init firebase and directory

---
+++ CS4520FinalProject/CS4520FinalProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CS4520FinalProject
+//
+//  Created by Kenny Lin on 6/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
